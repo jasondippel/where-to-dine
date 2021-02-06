297f0cb08675a3976b8254f70d7c1ad69efc2345
Jason Dippel <dippelj12@gmail.com>
2021-02-06 15:54:23 -0800

    Enable adding things to list

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,27 @@
 import firebase from 'firebase/app'
+import { string } from 'prop-types'
+import { useState } from 'react'
 import 'firebase/firestore'
 import 'firebase/auth'
 import { useAuthState } from 'react-firebase-hooks/auth'
-// import { useCollectionData } from 'react-firebase-hooks/firestore'
+import { useCollectionData } from 'react-firebase-hooks/firestore'
 
-firebase.initializeApp({
-  apiKey: 'AIzaSyCZx_JTB3_VRnetn0mwy6DjBcTxehf670I',
-  authDomain: 'what-to-eat-55dd9.firebaseapp.com',
-  projectId: 'what-to-eat-55dd9',
-  storageBucket: 'what-to-eat-55dd9.appspot.com',
-  messagingSenderId: '985901119162',
-  appId: '1:985901119162:web:de60467fc28173a7e77483',
-  measurementId: 'G-0NM3JXL7CR',
-})
+if (!firebase.apps.length) {
+  firebase.initializeApp({
+    apiKey: 'AIzaSyCZx_JTB3_VRnetn0mwy6DjBcTxehf670I',
+    authDomain: 'what-to-eat-55dd9.firebaseapp.com',
+    projectId: 'what-to-eat-55dd9',
+    storageBucket: 'what-to-eat-55dd9.appspot.com',
+    messagingSenderId: '985901119162',
+    appId: '1:985901119162:web:de60467fc28173a7e77483',
+    measurementId: 'G-0NM3JXL7CR',
+  })
+} else {
+  firebase.app() // use if already initialized, otherwise get error about duplicate initialization
+}
 
 const auth = firebase.auth()
-// const firestore = firebase.firestore()
+const firestore = firebase.firestore()
 
 const SignIn = () => {
   const signInWithGoogle = () => {
@@ -41,9 +47,65 @@ const SignOut = () => {
   )
 }
 
+const Place = ({ name }) => <div>{name}</div>
+Place.propTypes = {
+  name: string,
+}
+
+const AddPlaceForm = () => {
+  const [formValue, setFormValue] = useState('')
+  const { uid } = auth.currentUser
+  const userPlacesRef = firestore.collection(`user-data/${uid}/places`)
+
+  const handleInputChnage = (e) => {
+    setFormValue(e.target.value)
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault() // prevent form submission from refreshing page
+    await userPlacesRef.add({
+      'created-at': firebase.firestore.FieldValue.serverTimestamp(),
+      name: formValue,
+      favourite: false,
+      visited: false,
+    })
+    setFormValue('')
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input value={formValue} onChange={handleInputChnage} />
+      <button type="submit">Add Place</button>
+    </form>
+  )
+}
+
+const Places = () => {
+  const [user] = useAuthState(auth)
+  const { uid } = user || {}
+  const userPlacesRef = firestore.collection(`user-data/${uid}/places`)
+  const query = userPlacesRef.orderBy('created-at', 'desc')
+  const [places] = useCollectionData(query, { idField: 'id' })
+
+  if (!user) return null
+
+  return <> {places && places.map((p) => <Place {...p} key={p.id} />)} </>
+}
+
 const App = () => {
   const [user] = useAuthState(auth)
-  return <div className="App">{user ? <SignOut /> : <SignIn />}</div>
+  return (
+    <div className="App">
+      {user ? <SignOut /> : <SignIn />}
+
+      {user && (
+        <>
+          <AddPlaceForm />
+          <Places />
+        </>
+      )}
+    </div>
+  )
 }
 
 export default App
